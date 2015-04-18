fb166544b305607296602399e275391c1f41abc9
Louis Tur <louis.tur@gmail.com>
2015-04-18 17:18:58 -0400

    Adds one moar pseudotest..

--- RNFrostedSidebarTests/RNFrostedSidebarSpecSpec.m
+++ RNFrostedSidebarTests/RNFrostedSidebarSpecSpec.m
@@ -16,14 +16,55 @@ SpecBegin(RNFrostedSidebarSpec)
 describe(@"+[RNFrostedSidebar visibleSidebar] returns instance of RNFrostedSidebar", ^{
     
     __block RNFrostedSidebar * frostedSidebar;
+    __block RNFrostedSidebar * designatedInitFrostedSidebar;
+    __block NSMutableIndexSet *optionIndices;
+    
+    NSArray *images = @[
+                        [UIImage imageNamed:@"gear"],
+                        [UIImage imageNamed:@"globe"],
+                        [UIImage imageNamed:@"profile"],
+                        [UIImage imageNamed:@"star"],
+                        [UIImage imageNamed:@"gear"],
+                        [UIImage imageNamed:@"globe"],
+                        [UIImage imageNamed:@"profile"],
+                        [UIImage imageNamed:@"star"],
+                        [UIImage imageNamed:@"gear"],
+                        [UIImage imageNamed:@"globe"],
+                        [UIImage imageNamed:@"profile"],
+                        [UIImage imageNamed:@"star"],
+                        ];
+    
+    NSArray *colors = @[
+                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+                        [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
+                        [UIColor colorWithRed:255/255.f green:137/255.f blue:167/255.f alpha:1],
+                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+                        ];
+    
     beforeAll(^{
+        optionIndices = [NSMutableIndexSet indexSetWithIndex:1];
+        
         frostedSidebar = [RNFrostedSidebar visibleSidebar];
+        designatedInitFrostedSidebar = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:optionIndices borderColors:colors];
+        
     });
-
     
     it(@"Should not be nil", ^{
         expect(frostedSidebar).toNot.beNil;
-    });  
+        expect(designatedInitFrostedSidebar).toNot.beNil;
+    });
+    
+    it(@"", ^{
+        
+    });
     
 });
 
